014310d186dd651f3a3ba5ee23c5d8a46cf1d43a
Victor ALAMO <alamov1@ict-2015-laptop.local>
2017-08-30 17:39:08 +0800

    Initial Commit

---
+++ SavetheKittenExcepttheHairyone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SavetheKittenExcepttheHairyone
+//
+//  Created by Victor ALAMO on 8/30/17.
+//  Copyright © 2017 Victor ALAMO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
